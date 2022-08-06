Updated nonparametric ddpg: fix bug with no buffer memory cycling, new entries are added to both current and target networks. Updated NeuralKNN: ability to turn on or off dictionary key-value gradients, initialize keys to infinity and values to zero. Added snapshot save ability to finetune script.

agent/np_ddpg.py
@@ -13,10 +13,12 @@ from modules.neural_dictionary import NeuralKNN
 
 class NonParametricCritic(nn.Module):
     def __init__(self, obs_type, obs_dim, action_dim,
-                 feature_dim, hidden_dim, value_head_cfg):
+                 feature_dim, hidden_dim, memory_overwrite,
+                 value_head_cfg, device):
         super().__init__()
 
         self.obs_type = obs_type
+        self.memory_overwrite = memory_overwrite
 
         if obs_type == 'pixels':
             # for pixels actions will be added after trunk
@@ -43,8 +45,8 @@ class NonParametricCritic(nn.Module):
         self.Q1_neck = make_q_neck()
         self.Q2_neck = make_q_neck()
 
-        self.Q1_head = hydra.utils.instantiate(value_head_cfg)
-        self.Q2_head = hydra.utils.instantiate(value_head_cfg)
+        self.Q1_head = hydra.utils.instantiate(value_head_cfg).to(device)
+        self.Q2_head = hydra.utils.instantiate(value_head_cfg).to(device)
 
         self.apply(utils.weight_init)
 
@@ -65,10 +67,16 @@ class NonParametricCritic(nn.Module):
     def add(self, obs, action, values):
         """Method to add entries to dictionary"""
         with torch.no_grad():
-            # TODO: temp solution to prevent circling back
-            if self.Q1_head.mem_size >= self.Q1_head.capacity:
-                return
+            # Methods for overwriting memory
+            if self.memory_overwrite == 'none':
+                if self.Q1_head.mem_size >= self.Q1_head.capacity:
+                    return  # Don't cycle back to overwrite previous entries
+            elif self.memory_overwrite == 'cyclic':
+                pass  # overwrite previous entries
+            else:
+                raise NotImplementedError
 
+            # Process inputs
             inpt = obs if self.obs_type == 'pixels' else torch.cat([obs, action],
                                                                    dim=-1)
             h = self.trunk(inpt)
@@ -95,91 +103,37 @@ class NonParametricCritic(nn.Module):
             return
 
 
-"""
-self,
-
-name, reward_free, obs_type, obs_shape, action_shape,
-device, lr, feature_dim, hidden_dim, critic_target_tau,
-num_expl_steps, update_every_steps, stddev_schedule, nstep,
-batch_size, stddev_clip, init_critic, use_tb, use_wandb, update_encoder, meta_dim=0
-"""
-
 class NonParamDDPGAgent(DDPGAgent):
-    def __init__(self, name, reward_free, obs_type, obs_shape, action_shape,
-                 device, lr, feature_dim, hidden_dim,
-                 critic_target_tau, num_expl_steps, update_every_steps,
-                 stddev_schedule, nstep, batch_size, stddev_clip,
-                 init_critic, double_q, value_head_cfg, mc_buffer_cfg,
-                 use_tb, use_wandb, update_encoder, meta_dim=0):
-        super().__init__(name, reward_free, obs_type, obs_shape, action_shape,
-                         device, lr, feature_dim, hidden_dim, critic_target_tau,
-                         num_expl_steps, update_every_steps, stddev_schedule,
-                         nstep, batch_size, stddev_clip, init_critic, use_tb,
-                         use_wandb, update_encoder, meta_dim=0)
+    def __init__(self, twin_q, value_head_cfg, mc_buffer_cfg, memory_overwrite,
+                 **kwargs):
+        super().__init__(**kwargs)
 
-        self.double_q = double_q
+        self.twin_q = twin_q
 
         self.critic = NonParametricCritic(
-            obs_type, self.obs_dim, self.action_dim, feature_dim,
-            hidden_dim, value_head_cfg
-        ).to(device)
+            self.obs_type, self.obs_dim, self.action_dim, self.feature_dim,
+            self.hidden_dim, memory_overwrite, value_head_cfg, self.device,
+        ).to(self.device)
         self.critic_target = NonParametricCritic(
-            obs_type, self.obs_dim, self.action_dim, feature_dim,
-            hidden_dim, value_head_cfg
-        ).to(device)
+            self.obs_type, self.obs_dim, self.action_dim, self.feature_dim,
+            self.hidden_dim, memory_overwrite, value_head_cfg, self.device,
+        ).to(self.device)
         self.critic_target.load_state_dict(self.critic.state_dict())
 
         # optimizers
-
-        if obs_type == 'pixels':
+        self.encoder_opt = None
+        if self.obs_type == 'pixels':
             self.encoder_opt = torch.optim.Adam(self.encoder.parameters(),
-                                                lr=lr)
-        else:
-            self.encoder_opt = None
-        self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=lr)
-        self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=lr)
+                                                lr=self.lr)
+
+        self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=self.lr)
+        self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
 
         self.train()
         self.critic_target.train()
 
         # Buffer for storing MC returns
         self.mc_buffer = utils.MCReturnBuffer(**mc_buffer_cfg)
-
-
-    def init_from(self, other):
-        # copy parameters over
-        utils.hard_update_params(other.encoder, self.encoder)
-        utils.hard_update_params(other.actor, self.actor)
-        if self.init_critic:
-            utils.hard_update_params(other.critic.trunk, self.critic.trunk)
-
-    def get_meta_specs(self):
-        return tuple()
-
-    def init_meta(self):
-        return OrderedDict()
-
-    def update_meta(self, meta, global_step, time_step, finetune=False):
-        return meta
-
-    def act(self, obs, meta, step, eval_mode):
-        obs = torch.as_tensor(obs, device=self.device).unsqueeze(0)
-        h = self.encoder(obs)
-        inputs = [h]
-        for value in meta.values():
-            value = torch.as_tensor(value, device=self.device).unsqueeze(0)
-            inputs.append(value)
-        inpt = torch.cat(inputs, dim=-1)
-        # assert obs.shape[-1] == self.obs_shape[-1]
-        stddev = utils.schedule(self.stddev_schedule, step)
-        dist = self.actor(inpt, stddev)
-        if eval_mode:
-            action = dist.mean
-        else:
-            action = dist.sample(clip=None)
-            if step < self.num_expl_steps:
-                action.uniform_(-1.0, 1.0)
-        return action.cpu().numpy()[0]
 
     def add(self, time_step_obj, meta, step):
         """
@@ -207,6 +161,7 @@ class NonParamDDPGAgent(DDPGAgent):
             obs_tensor, act_tensor, val_tensor = tensors
 
             self.critic.add(obs_tensor, act_tensor, val_tensor)
+            self.critic_target.add(obs_tensor, act_tensor, val_tensor)
 
         pass  # TODO; return metrics?
 
@@ -218,14 +173,14 @@ class NonParamDDPGAgent(DDPGAgent):
             dist = self.actor(next_obs, stddev)
             next_action = dist.sample(clip=self.stddev_clip)
             target_Q1, target_Q2 = self.critic_target(next_obs, next_action)
-            if self.double_q:
+            if self.twin_q:
                 target_V = torch.min(target_Q1, target_Q2)
             else:
                 target_V = target_Q1
             target_Q = reward + (discount * target_V)
 
         Q1, Q2 = self.critic(obs, action)
-        if self.double_q:
+        if self.twin_q:
             critic_loss = F.mse_loss(Q1, target_Q) + F.mse_loss(Q2, target_Q)
         else:
             critic_loss = F.mse_loss(Q1, target_Q)
@@ -246,34 +201,6 @@ class NonParamDDPGAgent(DDPGAgent):
             self.encoder_opt.step()
 
         return metrics
-
-    def update_actor(self, obs, step):
-        metrics = dict()
-
-        stddev = utils.schedule(self.stddev_schedule, step)
-        dist = self.actor(obs, stddev)
-        action = dist.sample(clip=self.stddev_clip)
-        log_prob = dist.log_prob(action).sum(-1, keepdim=True)
-        Q1, Q2 = self.critic(obs, action)
-        Q = torch.min(Q1, Q2)
-
-        actor_loss = -Q.mean()
-
-        # optimize actor
-        self.actor_opt.zero_grad(set_to_none=True)
-        actor_loss.backward()
-        self.actor_opt.step()
-
-        if self.use_tb or self.use_wandb:
-            metrics['actor_loss'] = actor_loss.item()
-            metrics['actor_logprob'] = log_prob.mean().item()
-            metrics['actor_ent'] = dist.entropy().sum(dim=-1).mean().item()
-
-        return metrics
-
-    def aug_and_encode(self, obs):
-        obs = self.aug(obs)
-        return self.encoder(obs)
 
     def update(self, replay_iter, step):
         metrics = dict()

finetune.py
@@ -70,6 +70,7 @@ class Workspace:
         # initialize from pretrained
         if cfg.snapshot_ts > 0:
             pretrained_agent = self.load_snapshot()['agent']
+            print(self.agent, pretrained_agent)
             self.agent.init_from(pretrained_agent)
             print('Loaded snapshop agent', pretrained_agent)
 
@@ -227,18 +228,29 @@ class Workspace:
             episode_reward += time_step.reward
             self.replay_storage.add(time_step, meta)
             self.agent.add(time_step, meta, self.global_step)  # NOTE: added this for online processing
+
             self.train_video_recorder.record(time_step.observation)
+            # try to save snapshot
+            if self.cfg.save_snapshots and self.global_frame in self.cfg.snapshots:
+                self.save_snapshot()
+
             episode_step += 1
             self._global_step += 1
 
     def load_snapshot(self):
         snapshot_base_dir = Path(self.cfg.snapshot_base_dir)
-        domain, _ = self.cfg.task.split('_', 1)
-        snapshot_dir = snapshot_base_dir / self.cfg.obs_type / domain / self.cfg.agent.name
+        if self.cfg.snapshot_child_dir is None:
+            domain, _ = self.cfg.task.split('_', 1)
+            snapshot_dir = snapshot_base_dir / self.cfg.obs_type / domain / self.cfg.agent.name
+        else:
+            snapshot_dir = snapshot_base_dir / Path(self.cfg.snapshot_child_dir)
 
         def try_load(seed):
-            snapshot = snapshot_dir / str(
-                seed) / f'snapshot_{self.cfg.snapshot_ts}.pt'
+            if self.cfg.snapshot_child_dir is None:
+                snapshot = snapshot_dir / str(
+                    seed) / f'snapshot_{self.cfg.snapshot_ts}.pt'
+            else:
+                snapshot = snapshot_dir / f'snapshot_{self.cfg.snapshot_ts}.pt'
             if not snapshot.exists():
                 return None
             with snapshot.open('rb') as f:
@@ -260,6 +272,16 @@ class Workspace:
         # otherwise throw error
         ssdp = snapshot_dir / '[1-11]' / f'snapshot_{self.cfg.snapshot_ts}.pt'
         raise RuntimeError(f'Did not find snapshot at: {ssdp}')
+
+    def save_snapshot(self):
+        # TODO: see this works still
+        snapshot_dir = self.work_dir / Path(self.cfg.snapshot_out_dir)
+        snapshot_dir.mkdir(exist_ok=True, parents=True)
+        snapshot = snapshot_dir / f'snapshot_{self.global_frame}.pt'
+        keys_to_save = ['agent', '_global_step', '_global_episode']
+        payload = {k: self.__dict__[k] for k in keys_to_save}
+        with snapshot.open('wb') as f:
+            torch.save(payload, f)
 
 
 @hydra.main(config_path='.', config_name='finetune')

modules/neural_dictionary.py
@@ -29,9 +29,8 @@ class BaseScoreFunction(nn.Module):
 class CosineSimilaritySF(BaseScoreFunction):
     """Cosine similarity score function"""
 
-    def __init__(self, feature_dims):
+    def __init__(self, eps=1e-6):
         super(CosineSimilaritySF, self).__init__()
-        self.feature_dims = feature_dims
         self.eps = 1e-6  # numerical stability
 
     def forward(self, mat1, mat2):
@@ -49,18 +48,18 @@ class CosineSimilaritySF(BaseScoreFunction):
 
 
 class NeuralDictionary(nn.Module):
-    def __init__(self, capacity, key_dim):
+    def __init__(self, key_dim, value_dim, capacity, keys_grad, values_grad):
         super(NeuralDictionary, self).__init__()
         self.capacity = capacity
         self.key_dim = key_dim
-        self.value_dim = 1  # TODO: maybe make this customizable?
+        self.value_dim = value_dim
 
         self.keys = nn.parameter.Parameter(
             torch.empty((capacity, self.key_dim)),
-            requires_grad=True)
+            requires_grad=keys_grad)
         self.values = nn.parameter.Parameter(
             torch.empty(capacity, self.value_dim),
-            requires_grad=True)
+            requires_grad=values_grad)
 
         # TODO: initialize weights here or no?
         # nn.init.normal_(self.keys)
@@ -71,8 +70,9 @@ class NeuralDictionary(nn.Module):
 
 
 class SoftNeuralDictionary(NeuralDictionary):
-    def __init__(self, capacity, key_dim, temperature, score_fn_cfg):
-        super(SoftNeuralDictionary, self).__init__(capacity, key_dim)
+    def __init__(self, temperature, score_fn_cfg,
+                 **kwargs):
+        super(SoftNeuralDictionary, self).__init__(**kwargs)
         self.temperature = temperature
 
         self.log_temp = nn.parameter.Parameter(
@@ -81,9 +81,10 @@ class SoftNeuralDictionary(NeuralDictionary):
         nn.init.zeros_(self.keys)
         nn.init.zeros_(self.values)
 
+
         self.score_fn = hydra.utils.instantiate(score_fn_cfg)
 
-        self.mem_idx = 0
+        self.mem_idx = 0  # TODO make multi dimensional?
         self.mem_size = 0
 
     def forward(self, queries):
@@ -113,20 +114,25 @@ class SoftNeuralDictionary(NeuralDictionary):
 
 
 class NeuralKNN(NeuralDictionary):
-    def __init__(self, capacity, key_dim, k_neighbours):
-        super(NeuralKNN, self).__init__(capacity, key_dim)
+    def __init__(self, k_neighbours, **kwargs):
+        super(NeuralKNN, self).__init__(**kwargs)
         self.k_neighbours = k_neighbours
+        # self.mem_idxes = torch.zeros((self.value_dim), dtype=torch.int)
+        # self.mem_sizes = torch.zeros((self.value_dim), dtype=torch.int)
         self.mem_idx = 0
         self.mem_size = 0
+
+        nn.init.constant_(self.keys, float("Inf"))
+        nn.init.constant_(self.values, 0.)
 
         self.eps = 1e-8
 
     def forward(self, queries):
-        # TODO: dummy variable, fix this and use just a single one
         return self.forward_matmul(queries)
 
     def forward_matmul(self, queries):
         """
+        Old, deprecating the
         Compute k-nn value estimates, implemented from scratch
         :param queries: state or minibatches of states, shape (B, key_dim)
         :return: value vector of size (B, value_dim)