720138433d5326b9161807ca4ff1bf48ff4eca59
Anthony <26030673+im-ant@users.noreply.github.com>
2022-08-09 18:15:55 -0400

    added simple gaussian process value function to neural dictionary

---
+++ modules/gaussian_process.py
@@ -0,0 +1,80 @@
+# ===
+#
+# ===
+import hydra
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from modules.neural_dictionary import NeuralDictionary
+
+
+class SEKernel(nn.Module):
+    """
+    Squared Exponential (SE) Kernel
+    """
+    def __init__(self, lengthscale, train_lengthscale):
+        super(SEKernel, self).__init__()
+
+        self.lengthscale = nn.parameter.Parameter(
+            torch.tensor(float(lengthscale)), requires_grad=train_lengthscale)
+        # TODO: make sure it is on the right device
+        # TODO: do we need a scaling factor of a?
+
+    def forward(self, x1, x2):
+        """
+        :param x1: tensor of shape (n, d)
+        :param x2: tensor of shape (m, d)
+        :return: tensor of shape (n, m) of their covariance matrix
+        """
+        euDist = torch.cdist(x1, x2, p=2.0)  # (n, m)
+        kMat = torch.exp(-euDist / (2 * (self.lengthscale**2)))  # (n, m)
+        return kMat
+
+
+class IIDGaussianNoiseGP(NeuralDictionary):
+    def __init__(self, noise_var, kernel_fn_cfg,
+                 **kwargs):
+        super(IIDGaussianNoiseGP, self).__init__(**kwargs)
+
+        self.kernel = hydra.utils.instantiate(kernel_fn_cfg)
+        self.noise_var = noise_var  # TODO: make this trainable parameter
+
+        self.KMat = None
+        self.invKIMat = None
+
+        # TODO: add bias, and what is the prior function?
+
+        self.mem_idx = 0  # TODO make multi dimensional?
+        self.mem_size = 0
+        # TODO: pre-compute the inverse covariance
+
+    def _precompute_covariances(self):
+        """
+        Helper function to optionally pre-compute the vocariance matrix
+        """
+        KMat = self.kernel(self.keys[:self.mem_size, :],
+                           self.keys[:self.mem_size, :])  # (M, M)
+        iMat = torch.eye(self.mem_size, device=KMat.device)
+
+        invKIMat = torch.linalg.inv(KMat + (self.noise_var * iMat))
+
+        return KMat, invKIMat
+
+    def inplace_compute_covariances(self):
+        KMat, invKIMat = self._precompute_covariances()
+        self.KMat, self.invKIMat = KMat, invKIMat
+
+    def forward(self, queries):
+        """
+        :param queries: state or minibatches of states, shape (B, key_dim)
+        :return: mean prediction of y, shape (B, value_dim)
+        """
+        self.inplace_compute_covariances()  # TODO: do this every step?
+        if self.invKIMat == None:
+            raise Exception('inverse covariance matrix is not initialized')
+
+        kstar = self.kernel(queries, self.keys[:self.mem_size, :])  # (B, M)
+        y_mean = kstar @ self.invKIMat @ self.values[:self.mem_size,:]  # (B, ?)
+        return y_mean
